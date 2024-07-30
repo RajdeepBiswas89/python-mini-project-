c6663fa8756aef5895bda8d93c745871243cbea4
RajdeepBiswas89 <rajdeepbiswas403@gmail.com>
2024-07-30 12:16:00 +0530

    Drink water reminder using plyer module

---
+++ drink_water_reminder.py
@@ -0,0 +1,8 @@
+from plyer import notification
+import time
+notification.notify(
+        title="Drink water",
+        message="it has been too long drink water",
+        timeout=1
+      )
+time.sleep(5)
